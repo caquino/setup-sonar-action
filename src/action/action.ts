3d83a576ff61dfc4d6407b1746b947895acfa033
Cassiano Aquino <cassiano.aquino@me.com>
2024-07-09 20:42:29 +0100

    add debug

--- src/action/action.ts
+++ src/action/action.ts
@@ -23,17 +23,19 @@ export async function run() {
       core.setOutput(ActionOutputKeys.organization, projectExists.organization);
       core.setOutput(ActionOutputKeys.projectKey, projectExists.key);
 
-      core.notice(
+      console.log(
         `Project ${projectExists.key} already exists. No action performed.`
       );
       return core.ExitCode.Success;
     }
 
+    console.log(`Creating project ${createProjectParams.project}...`);
     const { project } = await api.createProject(createProjectParams);
 
     const shouldRenameMainBranch = inputs.mainBranch !== 'master';
 
     if (shouldRenameMainBranch) {
+      console.log(`Renaming main branch to ${inputs.mainBranch}...`);
       await api.renameMasterBranch({
         name: inputs.mainBranch,
         project: project.key,
@@ -41,6 +43,7 @@ export async function run() {
     }
 
     if (!inputs.autoScan) {
+      console.log(`Disabling autoscan for project ${project.key}...`);
       await api.disableAutoscan({
         projectKey: project.key,
         enabled: false,
